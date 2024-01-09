f0568b216bc24681a0963700e0b2e86b85dd3dc7
Munkhuu-u <152163557+Munkhuu-u@users.noreply.github.com>
2024-01-09 12:53:07 +0800

    calculator init

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import { Container } from "./components/Container";
+
+export default function App() {
+  return (
+    <div>
+      <p> App title</p>
+      <Container />
+    </div>
+  );
+}
---
+++ src/components/Container.jsx
@@ -0,0 +1,10 @@
+import Button from "./Button";
+
+export function Container() {
+  return (
+    <div className="container">
+      <p>Container title</p>
+      <Button />
+    </div>
+  );
+}
