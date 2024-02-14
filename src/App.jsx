d5a11263887271c1086d7f36183f4e20501e36a7
Munkhuu-u <152163557+Munkhuu-u@users.noreply.github.com>
2024-02-14 14:04:37 +0800

    all done except button push style

--- src/App.jsx
+++ src/App.jsx
@@ -11,55 +11,134 @@ import { useState } from "react";
 export default function App() {
   const [screenValue, setScreenValue] = useState("");
   const [num1, setNum1] = useState("");
+  const [num2, setNum2] = useState("");
   const [operator, setOper] = useState("");
+  const [opClick, setOpClick] = useState("operator");
+  // const [opChanged, setOpChanged] = useState("");
 
-  function num1Handler(changes) {
-    setScreenValue(screenValue + changes + "");
+  function numHandler(changes) {
+    if (operator == "" && num2 == "") {
+      setScreenValue(screenValue + changes + "");
+      console.log(
+        "screenValue: ",
+        screenValue,
+        "\n",
+        "num1: ",
+        num1,
+        "\n",
+        "num2: ",
+        num2,
+        "\n",
+        "ope: ",
+        operator
+      );
+    } else if (operator != "" && num2 == "") {
+      setScreenValue(changes);
+      setNum2(changes);
+    } else {
+      setScreenValue(screenValue + changes + "");
+      setNum2(screenValue + changes + "");
+    }
   }
 
   function opHandler(op) {
     if (op === "=") {
       equalHandler(operator);
     } else {
+      equalHandler(operator);
       setNum1(screenValue);
       setOper(op);
-      console.log(op);
-      setScreenValue("");
     }
   }
 
   function equalHandler(operator) {
     switch (operator) {
       case "+":
-        setScreenValue(Number(num1) + Number(screenValue));
+        setScreenValue(
+          Math.round((Number(num1) + Number(num2)) * 10000000) / 10000000
+        );
+        setNum1(Number(num1) + Number(num2));
+        setNum2("");
+        console.log(
+          "screenValue: ",
+          screenValue,
+          "\n",
+          "num1: ",
+          num1,
+          "\n",
+          "num2: ",
+          num2,
+          "\n",
+          "ope: ",
+          operator
+        );
         break;
       case "-":
-        setScreenValue(Number(num1) - Number(screenValue));
+        setScreenValue(
+          Math.round((Number(num1) - Number(num2)) * 10000000) / 10000000
+        );
+        setNum1(Number(num1) - Number(num2));
+        setNum2("");
+        console.log(
+          "screenValue: ",
+          screenValue,
+          "\n",
+          "num1: ",
+          num1,
+          "\n",
+          "num2: ",
+          num2,
+          "\n",
+          "ope: ",
+          operator
+        );
         break;
       case "*":
-        setScreenValue(Number(num1) * Number(screenValue));
+        setScreenValue(
+          Math.round(Number(num1) * Number(num2) * 10000000) / 10000000
+        );
+        setNum1(Number(num1) * Number(num2));
+        setNum2("");
         break;
       case "/":
-        setScreenValue(Number(num1) / Number(screenValue));
+        setScreenValue(
+          Math.round((Number(num1) / Number(num2)) * 10000000) / 10000000
+        );
+        setNum1(Number(num1) / Number(num2));
+        setNum2("");
         break;
     }
   }
 
   function autoHandler(autoOperator) {
-    console.log("autoHandler ajillaj baina ");
     console.log("autoOperator:", autoOperator);
     switch (autoOperator) {
       case "C":
         setScreenValue("");
         setNum1("");
+        setNum2("");
         setOper("");
         break;
       case "±":
         setScreenValue(Number(screenValue) * -1);
+        setNum1(Number(screenValue) * -1);
         break;
       case "%":
-        console.log("ajillaj baina");
-        setScreenValue((num1 * screenValue) / 100);
+        console.log(
+          "screenValue: ",
+          screenValue,
+          "\n",
+          "num1: ",
+          num1,
+          "\n",
+          "num2: ",
+          num2,
+          "\n",
+          "ope: ",
+          operator
+        );
+        setScreenValue(screenValue / 100);
+        setNum1(screenValue / 100);
         break;
     }
   }
@@ -76,13 +155,18 @@ export default function App() {
           </div>
           <div className="numbers">
             {numbButtNames.map((e) => (
-              <ButtNumb name={e} funcNum={num1Handler} />
+              <ButtNumb name={e} funcNum={numHandler} />
             ))}
           </div>
         </div>
         <div className="operators">
           {operButtNames.map((e) => (
-            <ButtOper name={e} funcOpe={opHandler} />
+            <ButtOper
+              name={e}
+              funcOpe={opHandler}
+              opClick={opClick}
+              setOpClick={setOpClick}
+            />
           ))}
         </div>
       </div>
--- src/components/ButtOper.jsx
+++ src/components/ButtOper.jsx
@@ -1,11 +1,14 @@
 import "../styles/buttOper.css";
+// import React, { useState } from "react";
 
-export default function ButtOper({ name, funcOpe }) {
+export default function ButtOper({ name, funcOpe, opClick, setOpClick }) {
+  // const [push, setPush] = useState("operator");
   return (
     <button
-      className="operator"
+      className={`${opClick}`}
       onClick={(e) => {
         funcOpe(name);
+        // setOpClick(opClick === "operator" ? "operatorPushed" : "operator");
       }}
     >
       {name}
